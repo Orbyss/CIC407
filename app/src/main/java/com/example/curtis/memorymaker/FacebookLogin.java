cf88b8466731cb4ec8a79940b3ea02a449131f04
Curtis <cutwen22s@gmail.com>
2017-03-15 17:39:44 -0700

    Some changes

---
+++ app/src/main/java/com/example/curtis/memorymaker/FacebookLogin.java
@@ -0,0 +1,133 @@
+package com.example.curtis.memorymaker;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+
+/**
+ * Created by Coryna on 2/22/2017.
+ */
+
+public class FacebookLogin extends Activity
+{
+    CallbackManager callbackManager;
+    View view;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_facebook_btn);
+        callbackManager = CallbackManager.Factory.create();
+        textView = (TextView)findViewById(R.id.info);
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+//                        textView.setText(
+//                                "User ID: "
+//                                        + loginResult.getAccessToken().getUserId()
+//                                        + "\n" +
+//                                        "Auth Token: "
+//                                        + loginResult.getAccessToken().getToken()
+//                        );
+
+//                        textView.setText("Name: " + loginResult.getAccessToken().getUserId().getClass());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+    }
+
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.test_facebook_btn, container, false);
+        textView = (TextView)findViewById(R.id.info);
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+
+        // If using in a fragment
+//        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                // App code
+                textView.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+
+            }
+
+            @Override
+            public void onCancel()
+            {
+                // App code
+                textView.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException exception)
+            {
+                // App code
+                textView.setText("Login attempt failed.");
+            }
+        });
+
+
+        return view;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
+
+
+
